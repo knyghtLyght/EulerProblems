3a9bac81c7c7cdb73e31719877daa3c06728efbc
Kevin <kevnaf@gmail.com>
2017-04-05 17:52:46 -0700

    04.05.17.office

---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+            string answer = "";
+            while (input <= 1000)
+            {
+                if (input % 3 == 0 || input % 5 == 0)
+                {
+                    answer = answer + " " + input.ToString();
+                }
+                input++;
+            }
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problem3/Problem3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            The prime factors of 13195 are 5, 7, 13 and 29.
+            What is the largest prime factor of the number 600851475143?
+            */
+        }
+
+        static int GetLargestFactor(long numberToCheck)
+        {
+            int sqrt = (int)Math.Ceiling(Math.Sqrt(numberToCheck)); //Check how big our factors can get and maybe get lucky.
+            if (sqrt * sqrt == numberToCheck)
+            {
+                if (isPrime(sqrt)) return sqrt;
+            }
+            for (int i = sqrt - 1; i > 0; i--)
+            {
+                if (sqrt % i == 0)
+                {
+                    if (isPrime(i)) return i;
+                }
+            }
+            return 0;
+        }
+        static bool isPrime(long number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            var boundry = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 2; i <= boundry; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
